19062491f72e087c188be66ec750fa7ecf666780
Meghann McCool Walsh <meghann.mccool.walsh@gmail.com>
2019-07-03 20:27:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languges]
+		 programmer_hash[:dennis_ritchie][:languges][0]
 end
 
 def adding_matz
